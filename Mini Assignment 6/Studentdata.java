848d1325e6e442d9d65ab4d791c9e2197b52bb27
tejasmanu-deloitte <tejasmas@deloitte.com>
2022-03-21 13:51:39 +0530

    Add files via upload

---
+++ Mini Assignment 6/Studentdata.java
@@ -0,0 +1,29 @@
+package File_Handling;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Studentdata {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(new FileReader("C:\\Users\\tejasmas\\AppData\\Roaming\\JetBrains\\IdeaIC2021.3\\scratches\\Student\\StudentData.csv"));
+            List<String> ls = new ArrayList<>();
+            int i = 0;
+            while (sc.hasNext()) {
+                String[] str = sc.nextLine().split(",");
+                if (i != 0) {
+                    if (Integer.parseInt(str[4])>=1 && Integer.parseInt(str[4])<=5) {
+                        ls.add(new String(str[0]+" "+ str[1]+" "+ str[2]+" "+ str[3]+" "+Integer.parseInt(str[4])));
+                        System.out.println(str[0] + " " + str[1] + " " + str[2] + " " + str[3] + " " + str[4]);
+                    }
+                }
+                i++;
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+    }
+}
